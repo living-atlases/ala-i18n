212f95a799f2a2c8fb697db7068dca4098a7669d
vjrj <vjrj@ourproject.org>
2020-04-08 21:06:42 +0200

    Added small script to maintain a local version configuration of each crowdin.yml

---
+++ update-crowdin-in-submodules.sh
@@ -0,0 +1,4 @@
+for i in collectory-plugin biocache-service biocache-hubs bie-plugin bie-index userdetails specieslist-webapp spatial-service spatial-hub regions downloads-plugin
+do
+    cp crowdin-configs/$i-crowdin.yml $i/crodwin.yml
+done
