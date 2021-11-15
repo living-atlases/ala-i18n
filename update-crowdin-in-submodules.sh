eb7ed6c740abc3d89d73190409a2b7c56d2b3b02
vjrj <vjrj@ourproject.org>
2021-11-15 08:48:51 +0100

    Added doi crowdin config

--- update-crowdin-in-submodules.sh
+++ update-crowdin-in-submodules.sh
@@ -1,4 +1,4 @@
-for i in alerts ala-cas-5 collectory-plugin biocache-service biocache-hubs bie-plugin bie-index image-service userdetails specieslist-webapp spatial-service spatial-hub regions downloads-plugin
+for i in alerts ala-cas-5 collectory-plugin biocache-service biocache-hubs bie-plugin bie-index doi-service image-service userdetails specieslist-webapp spatial-service spatial-hub regions downloads-plugin
 do
     cp crowdin-configs/$i-crowdin.yml $i/crowdin.yml
 done
