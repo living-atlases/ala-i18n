771079aaa2c985f3764d9a3716dd76b943916724
vjrj <vjrj@ourproject.org>
2022-04-04 12:24:25 +0200

    move collectory-plugin to collectory

--- update-crowdin-in-submodules.sh
+++ update-crowdin-in-submodules.sh
@@ -1,4 +1,4 @@
-for i in alerts ala-cas-5 collectory-plugin biocache-service biocache-hubs bie-plugin bie-index doi-service image-service userdetails specieslist-webapp spatial-service spatial-hub regions downloads-plugin ecodata biocollect
+for i in alerts ala-cas-5 collectory biocache-service biocache-hubs bie-plugin bie-index doi-service image-service userdetails specieslist-webapp spatial-service spatial-hub regions downloads-plugin ecodata biocollect
 do
     cp crowdin-configs/$i-crowdin.yml $i/crowdin.yml
 done
